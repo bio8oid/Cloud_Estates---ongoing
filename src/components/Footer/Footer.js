f1c7c55acc3a51fa6d8edd9ce3a26439ba895c26
bio8oid <8bollod8@mail.co.uk>
2020-05-12 14:24:45 +0200

    Add footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,34 +4,37 @@ import { theme } from '../../utils/theme';
 
 
 const StyledFooter = styled.div`
-height: 50px;
+${theme.flex.centered};
 width: 30%;
 background: black;
 opacity: .8;
 position: absolute;
 bottom: 5%;
 left: 0;
-
-h3 {
-    font-size: 2rem;
-    color: white;
-    text-align: center;
-    font-family: ${theme.font.indie};
-    /* position: relative; */
-}
 border-top-right-radius: 10px;
 border-bottom-right-radius: 10px;
-`;
 
+p {
+    ${theme.font.indie};
+    color: white;
+    font-size: 1.5rem;
+    margin: 0;
+    padding: 10px;
+}
+
+    ${theme.media.mobile} {
+    width: 100%;       
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;      
+}
+`;
 
 const Footer = () => (
     <>
         <StyledFooter>
-            <h3>bio8oid © </h3>
+            <p>bio8oid © { new Date().getFullYear() }</p>
         </StyledFooter>
     </>
 )
 
 export default Footer;
-
-// { new Date().getFullYear() }
