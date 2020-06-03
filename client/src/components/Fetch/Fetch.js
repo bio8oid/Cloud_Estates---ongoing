c6739bb26a07f077cab206f7c5e515f1778e5e83
bio8oid <8bollod8@mail.co.uk>
2020-06-03 19:58:29 +0200

    Nothing to commit

--- client/src/components/Fetch/Fetch.js
+++ client/src/components/Fetch/Fetch.js
@@ -28,20 +28,56 @@ import React, { useState, useEffect } from "react";
 
 
 
-const useFetch = async (query, string) => {
+const useFetch = (query, string) => {
+console.log('setTag:', string)
+
     const [pageContent, setPageContent] = useState([]);
     // const [error, setError] = useState(null);
+    const [routeTag, setRouteTag] = useState("");
     const [loading, setLoading] = useState(true)
-    
-    // const tagData = stringData !== null ? stringData.route : "";
+
+    // useEffect(() => {
+    //     setRouteTag(string);
+    // }, [string]);
+
+    // useEffect(() => {
+    //     const stringData = async ( ) => {
+            
+    //         console.log('I am string:', string)
+    //         if (string !== null) {
+    //             let tagData = string;
+    //             return tagData
+    //         }
+    //         console.log('I am string:', string)
+    //         await setRouteTag(string)
+            
+    //     };
+
+    //         stringData();
+    //     }, []);
+    //     console.log('stringData:', stringData())
+
+
+
+
+
+    let anyData = `{ "id": "7" }`
+    // let anyData = `{"tag": ${string}}`
+    console.log('anyData:', anyData)
+
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify({ query, variables: { string } }),
+        body: JSON.stringify({ query, variables: {anyData}  }),
     }
+
+
+    // const body =  JSON.stringify({ query, variables: { string } })
+
+    // console.log('routeTagAfter fetch:', routeTag)
 
     //     const headers = {
     //     'Content-Type': 'application/json',
@@ -50,28 +86,37 @@ const useFetch = async (query, string) => {
 
     const url = "http://localhost:8080/graphql";
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
+    useEffect(() => {
+        const fetchData = async () => {
             try {
-                // const tag = await tagData;
-            // let string = `{"tag": ${tag}}`
-            // const body = JSON.stringify({ query, variables: { string } });
-            const res = await fetch(url, options);
+
+                // const tag = await string
+                // setRouteTag(tag);
+
+                // const tag = routeTag;
+                // const tag = await stringData();
+                // console.log('tag:', tag)
+                // let stringgy = `{"tag": "gift"}`
+                // let stringgy = `{"tag": ${tag}}`
+                // const body = JSON.stringify({ query, variables: { stringgy } });
+                // const res = await fetch(url, { method: 'POST', headers: headers, body: body });
+                
+                const res = await fetch(url, options);
                 const json = await res.json();
-                console.log('json:', json)
-                setPageContent(json.data);
-                setLoading(false)
+                console.log('json:', json.data.properties);
+                // setPageContent(json.data);
+                // setLoading(false)
             } catch (error) {
                 // setError(error);
                 console.log(error);
             }
-        // };
-    //     fetchData();
-    // }, []);
-    return { pageContent, loading };
+        };
+        fetchData();
+    }, []);
+    // return { pageContent, loading };
+    return { pageContent, loading, routeTag };
 };
 
-export default useFetch;
 
 
 
@@ -92,22 +137,34 @@ export default useFetch;
 //         'Accept': 'application/json'
 //     };
 
-//     const tagData = stringData === null ? routeTag : props.location.state.route;
+//     // const tagData = stringData === null ? routeTag : props.location.state.route;
+//     const stringgy = string;
 
 //     const fetchData = async () => {
 //         try {
-//             const tag = await tagData;
-//             setRouteTag(tag);
+//             const tag = await stringgy;
+//             console.log('tag:', tag)
+//             // setRouteTag(tag);
 //             let string = `{"tag": ${tag}}`
+//             // let string = `{"tag": "gift"}`
 //             const body = JSON.stringify({ query, variables: { string } });
 //             const res = await fetch(url, { method: 'POST', headers: headers, body: body });
 //             const preData = await res.json();
-//             const data = preData.data
+//             const data = preData;
+//             console.log('data:', data);
 //             setPageContent(data);
-//             setLoading(false);
+//             // setLoading(false);
 //         } catch (err) {
 //             console.log(err);
 //         }
 //     }
 //     fetchData();
 // }, []);
+// }
+
+
+
+
+
+
+export default useFetch;
--- client/src/pages/propertiesList.js
+++ client/src/pages/propertiesList.js
@@ -8,13 +8,7 @@ import CarouselComponent from "../components/PropertiesList/Carousel/Carousel"
 import Spinner from "../components/Spinner/Spinner"
 import cloud3 from "../images/cloud_3.webp"
 
-
-// import { useAsync } from 'react-async-hook';
-
-import { useAsync } from "react-async"
-// import { useFetch } from "react-async"
-
-// import useFetch from "../components/Fetch/Fetch"
+import useFetch from "../components/Fetch/Fetch"
 // ---- List Styles ----
 
 const StyledWrapper = styled.div`
@@ -25,7 +19,6 @@ background: url(${cloud3}) no-repeat center;
 ${theme.responsiveImg};
 min-height: 90vh;
 `
-
 const Title = styled.h1`
 font-size: 4rem;
 opacity: .6;
@@ -41,24 +34,24 @@ padding: 0 5%;
 
 
 const PropertiesList = props => {
-console.log('props:', props)
 
     // const [loading, setLoading] = useState(true)
     // const [pageContent, setPageContent] = useState({ properties: [{}] });
-    const [routeTag, setRouteTag] = useState(null);
-    console.log('routeTag:', routeTag)
+    const [routeTaggy, setRouteTaggy] = useState("");
+    console.log('routeTaggy:', routeTaggy)
+    // console.log('routeTag:', routeTag)
     // const [pageContent, loading] = useFetch(query, string);
     // console.log('pageContent:', pageContent)
 
-
-   useEffect(() => {
-    const setTag = async () => {
-        const tag = await props.location.state.route;
-        setRouteTag(tag)
-    }
-       setTag();
-    },[]);
-
+    // useEffect(() => {
+        // const setTag = async () => {
+        //     const tag = await props.location.state.route;
+        //     return tag;
+        // }
+    //     setTag();
+    // }, []);
+    
+    // console.log('tag:', tag)
 
     const query = `query Properties($string: String) {
             properties(filter: $string) {
@@ -70,190 +63,89 @@ console.log('props:', props)
             }
         }`;
 
-  
-        let string = `{"tag": ${string}}`
-
-
-
-
-
-    // You can use async/await or any function that returns a Promise
-    const loadPlayer = async (query,  string ) => {
-   const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({ query, variables:  "vip"  })
-        }
-
-        const url = "http://localhost:8080/graphql";
-
-        const res = await fetch(url, options)
-        if (!res.ok) throw new Error(res.statusText)
-        return res.json()
-    }
-
-
-    const { data, error, isPending } = useAsync(loadPlayer)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     const MyComponent = (query, string) => {
-
-
-
-//    const options = {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Accept': 'application/json'
-//             },
-//             body: JSON.stringify({ query, variables:  "vip"  })
-//         }
-
-//         const url = "http://localhost:8080/graphql";
-
-//     // const headers = {
-//     //     'Content-Type': 'application/json',
-//     //     'Accept': 'application/json'
-//     // };
-
-//         const { data, error, isPending, run } = useFetch(url, options)
-//         // This will setup a promiseFn with a fetch request and JSON deserialization.
-
-//         // const res = await fetch(url, { method: 'POST', headers: headers, body: body });
-//         console.log(data)
-//         console.log(error)
-//         console.log(isPending)
-
-//         // you can later call `run` with an optional callback argument to
-//         // last-minute modify the `init` parameter that is passed to `fetch`
-//         function clickHandler() {
-//             run(init => ({
-//                 ...init,
-//                 headers: {
-//                     ...init.headers,
-//                     authentication: "...",
-//                 },
-//             }))
-//         }
-  
-
-//         // alternatively, you can also just use an object that will be spread over `init`.
-//         // please note that this is not deep-merged, so you might override properties present in the
-//         // original `init` parameter
-//     //     function clickHandler2() {
-//     //         run({ body: JSON.stringify(formValues) })
-//     //     }
-//     }
-//       MyComponent();
-
-    // console.log(clickHandler())
-    // console.log(clickHandler2())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // const keptTag = localStorage.getItem('keptRouteTag');
     // if (keptTag) setRouteTag(JSON.parse(localStorage.getItem('keptRouteTag')));
 
-
-
-    // useEffect(() => {
-    //     const setTag = async () => {
-    //         const tag = await props.location.state === null ? JSON.parse(localStorage.getItem('keptRouteTag')) : props.location.state.route;
-    //         console.log('tag:', tag)
-    //         setRouteTag(tag)
-    //         localStorage.setItem('keptRouteTag', JSON.stringify(tag))
-    //         var updatedTag = await routeTag;
-    //     }
-    //     setTag();
-    // }, []);
     
-    // const res =  useFetch(query, updatedTag);
+    useEffect(() => {
+        const setTag = async () => {
+            const tag = await props.location.state === null ? JSON.parse(localStorage.getItem('keptRouteTag')) : props.location.state.route;
+            console.log('tag:', tag);
+            setRouteTaggy(tag);
+            localStorage.setItem('keptRouteTag', JSON.stringify(tag));
+        }
+        setTag();
+    }, []);
 
     // const tag = props.location.state === null ? JSON.parse(localStorage.getItem('keptRouteTag')) : props.location.state.route;
-    //  console.log('tag:', tag)
-    //  setRouteTag(tag)
-    // localStorage.setItem('keptRouteTag', JSON.stringify(tag))
 
+    // const setContent = async () => {
     // console.log('routeTagAfter:', routeTag)
+    // let string = `{"tag": ${routeTag}}`
+    // let string = `{"tag": ${tag}}`
 
-    // const getget = (routeTag) => { 
-    //         if (routeTag !== null) {
-    //             return `{"tag": ${routeTag}}`
-    //         }
-    // } 
+    console.log('routeTaggyAfter:', routeTaggy)
 
-    // const taggy = useAsync(getget, routeTag)
-    // console.log('taggy:', taggy)
-    // let string = `{"tag": ${taggy}}`
+    const res = useFetch(query, routeTaggy);
 
-    // const [pageContent, loading] = useFetch(query, string);
+    // const loading = res.loading;
+    // const routeTag = res.routeTag;
+    // const pageContent = res.pageContent.properties;
+
+    // console.log('res:', res)
+    // console.log('loading:', loading)
     // console.log('pageContent:', pageContent)
 
-//     const asyncHero = useAsync(routeTag);
-//     // const res = useFetch(query, asyncHero);
-//     // console.log('res:', res)
-//   console.log('asyncHero:', asyncHero.result)
+
+    return (
+
+        <Layout>
+            <SEO title="Properties" />
+            <StyledBackground>
+                <StyledWrapper>
+
+                   {/* <Title>Welcome to {routeTag.replace(/["]/g, "")} Products List</Title> */}
+
+                    {/* {loading ? <Title>Welcome to {routeTag.replace(/["]/g, "")} Products List</Title> : "Loadimg..."} */}
+                    {/* <Title>Welcome to {tag.replace(/["]/g, "")} Products List</Title> */}
+
+                    {/* {loading ? <Spinner /> : <CarouselComponent pageContent={pageContent} state={{ route: routeTag }} />} */}
+
+                    {/* <ArrowButton state={{ route: routeTag, pathname: "/" }} /> */}
+                </StyledWrapper>
+            </StyledBackground>
+        </Layout>
+    )
+}
+
+export default PropertiesList;
 
 
-//   const loading = res.loading;
-//   const pageContent = res.pageContent.properties;
-//   console.log('res:', res)
-//   console.log('loading:', loading)
-//   console.log('pageContent:', pageContent)
+
+
+
+
+
+
+
+// useEffect(() => {
+//     const keptTag = localStorage.getItem('keptRouteTag');
+//     keptTag ? setRouteTag(JSON.parse(keptTag));
+// })
+
+// useEffect(() => {
+//     localStorage.setItem('keptRouteTag',JSON.stringify( routeTag))
+// })
+
+
+
+
 
     // useEffect(() => {
     //     const keptTag = localStorage.getItem('keptRouteTag');
     //     if (keptTag) setRouteTag(JSON.parse(keptTag));
     // },[]);
-    
+
     // console.log('routeTag2:', routeTag)
 
     // useEffect(() => {
@@ -299,37 +191,3 @@ console.log('props:', props)
     //     }
     //     fetchData();
     // }, []);
-
-  
-    return (
-
-        <Layout>
-            <SEO title="Properties" />
-            <StyledBackground>
-            <StyledWrapper>
-
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-
-
-
-                    {/* <Title>Welcome to {routeTag.replace(/["]/g, "")} Products List</Title>
-
-                {loading ? <Spinner /> : <CarouselComponent pageContent={pageContent} state={{ route: routeTag }} />}
-
-                <ArrowButton state={{ route: routeTag, pathname: "/" }}/> */}
-            </StyledWrapper>
-            </StyledBackground>
-        </Layout>
-    )
-}
-
-export default PropertiesList;
-
-// useEffect(() => {
-//     const keptTag = localStorage.getItem('keptRouteTag');
-//     keptTag ? setRouteTag(JSON.parse(keptTag));
-// })
-
-// useEffect(() => {
-//     localStorage.setItem('keptRouteTag',JSON.stringify( routeTag))
-// })
