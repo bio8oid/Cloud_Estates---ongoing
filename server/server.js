197a675145801f9ec53141f047e38bd9bc18e2f7
bio8oid <8bollod8@mail.co.uk>
2020-06-20 19:21:24 +0200

    Update server file

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(cors());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(helmet());
 
 
